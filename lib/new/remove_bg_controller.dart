47452dc86de92d46b7cb951cadd8d783dbc057b6
vaibhavt-nt <vaibhavti.nt@gmail.com>
2024-05-13 13:58:53 +0530

    add color and image to bg

--- lib/new/remove_bg_controller.dart
+++ lib/new/remove_bg_controller.dart
@@ -1,13 +1,15 @@
 import 'dart:typed_data';
+import 'package:bg_remove_without_package/new/snackbar.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
 
 class RemoveBgController extends GetxController {
   Uint8List? imageFile;
+  Uint8List? bgImageFile;
   String? imagePath;
   ScreenshotController controller = ScreenshotController();
   var isLoading = false.obs;
@@ -20,7 +22,7 @@ class RemoveBgController extends GetxController {
     request.files
         .add(await http.MultipartFile.fromPath("image_file", imagePath));
     request.headers.addAll(
-        {"X-API-Key": "xu6hJoouDagBYUmJmtpBk4sg"}); //XH5axMWfLx5SSxSFLqEvJcMA
+        {"X-API-Key": "XH5axMWfLx5SSxSFLqEvJcMA"}); //XH5axMWfLx5SSxSFLqEvJcMA
     final response = await request.send();
     if (response.statusCode == 200) {
       http.Response imgRes = await http.Response.fromStream(response);
@@ -29,6 +31,7 @@ class RemoveBgController extends GetxController {
       return imgRes.bodyBytes;
     } else {
       throw Exception("Error");
+      isLoading = false.obs;
     }
   }
 
@@ -53,14 +56,19 @@ class RemoveBgController extends GetxController {
   }
 
   void saveImage() async {
-    bool isGranted = await Permission.storage.status.isGranted;
-    if (!isGranted) {
-      isGranted = await Permission.storage.request().isGranted;
-    }
-    if (isGranted) {
-      String directory = (await getExternalStorageDirectory())!.path;
-      String fileName = "${DateTime.now().microsecondsSinceEpoch}.png";
-      controller.captureAndSave(directory, fileName: fileName);
+    if (imageFile != null) {
+      try {
+        final result = await ImageGallerySaver.saveImage(imageFile!);
+        if (result['isSuccess']) {
+          showSnackBar("Success", "Image saved successfully", false);
+        } else {
+          showSnackBar("Error", "Failed to save image", true);
+        }
+      } catch (e) {
+        showSnackBar("Error", "Failed to save image: $e", true);
+      }
+    } else {
+      showSnackBar("Error", "Please select an image", true);
     }
   }
 }
--- lib/new/remove_bg_screen.dart
+++ lib/new/remove_bg_screen.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: deprecated_member_use
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:bg_remove_without_package/new/choose_image.dart';
 import 'package:bg_remove_without_package/new/primanry_button.dart';
@@ -6,7 +7,10 @@ import 'package:bg_remove_without_package/new/remove_bg_controller.dart';
 import 'package:bg_remove_without_package/new/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class RemoveBackroundScreen extends StatefulWidget {
   const RemoveBackroundScreen({super.key});
@@ -16,10 +20,42 @@ class RemoveBackroundScreen extends StatefulWidget {
 }
 
 class _RemoveBackroundScreenState extends State<RemoveBackroundScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final ScreenshotController _screenshotController = ScreenshotController();
+  Color _backgroundColor = Colors.white; // Initial background color
+  Uint8List? _backgroundImage;
+
+  Future<void> _saveScreenshot() async {
+    try {
+      final image = await _screenshotController.capture();
+      await ImageGallerySaver.saveImage(image!);
+      showSnackBar("Success", 'Screenshot saved to gallery', false);
+    } catch (e) {
+      showSnackBar("Error", 'Failed to save screenshot: $e', true);
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _backgroundImage = File(pickedImage.path).readAsBytesSync();
+      });
+    }
+  }
+
+  void _changeBackgroundColor(Color color) {
+    setState(() {
+      _backgroundColor = color;
+    });
+  }
+
   bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: const Text('Remove Background'),
         ),
@@ -34,13 +70,60 @@ class _RemoveBackroundScreenState extends State<RemoveBackroundScreen> {
                       (controller.imageFile != null)
                           ? Column(
                               children: [
+                                // Color picker button using flex_color_picker package
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: const Text('Pick a color'),
+                                              content: SingleChildScrollView(
+                                                child: ColorPicker(
+                                                  pickerColor: _backgroundColor,
+                                                  onColorChanged:
+                                                      _changeBackgroundColor,
+                                                  pickerAreaHeightPercent: 0.8,
+                                                ),
+                                              ),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child: const Text('OK'),
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      },
+                                      child: const Text('Change BG Color'),
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: _pickImage,
+                                        child: const Text('Change BG image'))
+                                  ],
+                                ),
                                 Padding(
                                   padding: const EdgeInsets.symmetric(
                                       horizontal: 16),
                                   child: Screenshot(
-                                    controller: controller.controller,
-                                    child: Image.memory(
-                                      controller.imageFile!,
+                                    controller: _screenshotController,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: _backgroundColor,
+                                          image: DecorationImage(
+                                              image: MemoryImage(
+                                                  _backgroundImage!),
+                                              fit: BoxFit.cover)),
+                                      child: Image.memory(
+                                        controller.imageFile!,
+                                      ),
                                     ),
                                   ),
                                 ),
@@ -64,36 +147,36 @@ class _RemoveBackroundScreenState extends State<RemoveBackroundScreen> {
                                                     "Please select an image",
                                                     true);
                                               } else {
-                                                setState(() {
-                                                  isLoading = true;
-                                                });
-                                                controller.imageFile =
-                                                    await RemoveBgController()
-                                                        .removeBg(controller
-                                                            .imagePath!);
-                                                debugPrint("Success");
-                                                setState(() {
-                                                  isLoading = false;
-                                                });
+                                                try {
+                                                  setState(() {
+                                                    isLoading = true;
+                                                  });
+                                                  controller.imageFile =
+                                                      await RemoveBgController()
+                                                          .removeBg(controller
+                                                              .imagePath!);
+                                                  debugPrint("Success");
+                                                } catch (e) {
+                                                  // Handle error here
+                                                  debugPrint("Error: $e");
+                                                  showSnackBar(
+                                                      "Error", '$e', true);
+                                                  // Maybe show a snackbar for error handling
+                                                } finally {
+                                                  setState(() {
+                                                    isLoading = false;
+                                                  });
+                                                }
                                               }
-                                              controller.update();
-                                            }),
+                                            },
+                                          ),
                                 const SizedBox(height: 20),
                                 ReusablePrimaryButton(
-                                    childText: "Save Image",
-                                    textColor: Colors.white,
-                                    buttonColor: Colors.deepPurpleAccent,
-                                    onPressed: () async {
-                                      if (controller.imageFile != null) {
-                                        controller.saveImage();
-
-                                        showSnackBar("Success",
-                                            "Image saved successfully", false);
-                                      } else {
-                                        showSnackBar("Error",
-                                            "Please select an image", true);
-                                      }
-                                    }),
+                                  childText: "Save Image",
+                                  textColor: Colors.white,
+                                  buttonColor: Colors.deepPurpleAccent,
+                                  onPressed: _saveScreenshot,
+                                ),
                                 const SizedBox(height: 20),
                                 ReusablePrimaryButton(
                                     childText: "Add New Image",
